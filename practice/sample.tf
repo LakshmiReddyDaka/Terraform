447c86c10fdee3ef9c1b1930eaf43055563a811f
LakshmiReddyDaka <165203928+LakshmiReddyDaka@users.noreply.github.com>
2025-02-28 23:03:15 -0500

    Update sample.tf

--- practice/sample.tf
+++ practice/sample.tf
@@ -1,67 +1,20 @@
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
+# Specify the provider (AWS)
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-2"  # Change the region as per your requirement
 }
 
-resource "aws_vpc" "my_vpc" {
-   cidr_block = "10.0.0.0/16"
-}
+# Create an AWS EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"  # Instance type (t2.micro is eligible for the free tier)
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.my_vpc.id
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.my_vpc.cidr_block]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-}
-
-resource "aws_network_interface" "my_nic" {
-  subnet_id       = aws_subnet.my_subnet.id
-  private_ips     = ["10.0.1.50"]
-  security_groups = [aws_security_group.allow_tls.id]
-}
-
-resource "aws_instance" "my_instance" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
-  instance_type = "t2.micro"
-
-  network_interface {
-    network_interface_id = aws_network_interface.my_nic.id
-    device_index         = 0
-  }
-
-  credit_specification {
-    cpu_credits = "unlimited"
+  tags = {
+    Name = "MyFirstTerraformInstance"
   }
 }
+
+# Output the instance's public IP
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
+}
+
