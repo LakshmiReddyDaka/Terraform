126b05524f5364a3652726cb77a9f1de677dc283
LakshmiReddyDaka <165203928+LakshmiReddyDaka@users.noreply.github.com>
2025-03-01 14:31:43 -0500

    Update main.tf

--- practice/main.tf
+++ practice/main.tf
@@ -9,7 +9,7 @@ provider "google" {
 }
 
 # Example: Create a simple GCP Storage Bucket
-resource "google_storage_bucket" "terraformbucketmy12345" {
+resource "google_storage_bucket" "my-terraform-bucket-12345" {
   name     = "my-terraform-bucket-12345"
   location = "US"
   force_destroy = true
@@ -17,5 +17,5 @@ resource "google_storage_bucket" "terraformbucketmy12345" {
 
 # Output the bucket URL
 output "bucket_url" {
-  value = google_storage_bucket.example.url
+  value = google_storage_bucket.my-terraform-bucket-12345.url
 }
