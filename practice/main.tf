de1ad00441b729ab1ce36f747f729419be0b1b0c
LakshmiReddyDaka <165203928+LakshmiReddyDaka@users.noreply.github.com>
2025-03-01 12:34:37 -0500

    Create main.tf

---
+++ practice/main.tf
@@ -0,0 +1,21 @@
+# Define a variable to hold GCP credentials
+variable "GOOGLE_CREDENTIALS" {}
+
+# Google Cloud Provider Configuration
+provider "google" {
+  credentials = jsondecode(var.GOOGLE_CREDENTIALS)
+  project     = "winged-will-451921-k7"
+  region      = "us-central1" # Change to your preferred region
+}
+
+# Example: Create a simple GCP Storage Bucket
+resource "google_storage_bucket" "example" {
+  name     = "my-terraform-bucket-12345"
+  location = "US"
+  force_destroy = true
+}
+
+# Output the bucket URL
+output "bucket_url" {
+  value = google_storage_bucket.example.url
+}
