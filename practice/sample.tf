2f33b11cf28287c883e9f28288ea165a59f527f5
LakshmiReddyDaka <165203928+LakshmiReddyDaka@users.noreply.github.com>
2025-03-01 12:43:25 -0500

    Update sample.tf

--- practice/sample.tf
+++ practice/sample.tf
@@ -1,20 +1,2 @@
-# Specify the provider (AWS)
-provider "aws" {
-  region = "us-west-2"  # Change the region as per your requirement
-}
 
-# Create an AWS EC2 instance
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID for your region
-  instance_type = "t2.micro"  # Instance type (t2.micro is eligible for the free tier)
-
-  tags = {
-    Name = "MyFirstTerraformInstance"
-  }
-}
-
-# Output the instance's public IP
-output "instance_public_ip" {
-  value = aws_instance.example.public_ip
-}
 
